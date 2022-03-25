02cdb1ccbd9637738a342f1db0a6d7d8a7638ed1
ekram666 <ekramahmed2550@gmail.com>
2022-03-25 19:11:03 +0600

    another commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+
+    let total = 0;
+    let shipping = 0;
+
+    for(const product of cart)
+    {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat(total *0.1);
+    const grandTotal = total + shipping + tax;
     return (
-        <div>
-            <h3>Order summery</h3>
+        <div className='cart'>
+            <h5>Order summery</h5>
                 <p>Selected Item: {cart.length}</p>
+                <p>Total price: ${total} </p>
+                <p>Total Shipping: {shipping}</p>
+                <p>Tax: {tax}</p>
+                <h6>Grand Total: {grandTotal.toFixed(2)}</h6>
         </div>
     );
 };
